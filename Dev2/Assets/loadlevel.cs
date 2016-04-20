ad2fad9fc31c4ff8c5fd4f28dac5f190eb085f43
Chris Ward <Chris@Chriss-MacBook-Pro-2.local>
2016-04-20 14:02:36 +0100

    Final Commit
    Submission of LEGO Minimotion.

---
+++ Dev2/Assets/loadlevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class loadlevel : MonoBehaviour {
+
+	public void LoadStage0()  {
+
+		SceneManager.LoadScene ("jump");
+		//CameraFade.StartAlphaFade( Color.black, false, 0.5f, 0f, () => { SceneManager.LoadScene ("Scene0"); } );
+	}
+	public void LoadStage1()  {
+		//	CameraFade.StartAlphaFade( Color.black, false, 0.5f, 0f, () => { SceneManager.LoadScene ("LEGO_base"); } );
+		SceneManager.LoadScene ("run");
+	}
+
+	public void LoadStage2()  {
+		//	CameraFade.StartAlphaFade( Color.black, false, 0.5f, 0f, () => { SceneManager.LoadScene ("LEGO_base"); } );
+		SceneManager.LoadScene ("wave");
+	}
+
+	public void LoadStage3()  {
+		//	CameraFade.StartAlphaFade( Color.black, false, 0.5f, 0f, () => { SceneManager.LoadScene ("LEGO_base"); } );
+		SceneManager.LoadScene ("Drink");
+	}
+
+	public void LoadStage4()  {
+		//	CameraFade.StartAlphaFade( Color.black, false, 0.5f, 0f, () => { SceneManager.LoadScene ("LEGO_base"); } );
+		SceneManager.LoadScene ("Menu");
+	}
+
+}
